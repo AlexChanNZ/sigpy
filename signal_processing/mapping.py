Functionality: Added trailing edge to event mark wave. Changed normalisation method to sklearn fit_scaler. More events are now picked up (possibly more erroneous events too), but plots show more complete amplitudes.

signal_processing/mapping.py
@@ -1,7 +1,7 @@
 import config_global as cg
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
-
+from sklearn import preprocessing
 
 
 def get_grid() :
@@ -22,7 +22,8 @@ def map_channel_data_to_grid() :
 			chanNum = int(grid[r,c])
 			if (chanNum <= cg.dat['SigPy']['normData'].shape[0]) :
 				gridData[:,r,c] = cg.dat['SigPy']['normData'][(chanNum-1),:]
-	
+
+
 	return gridData
 
 
@@ -33,8 +34,6 @@ def map_event_data_to_grid() :
 
 	eventGrid = np.zeros(shape=(cg.dat['SigPy']['normData'].shape[1], grid.shape[0], grid.shape[1]), dtype=float)
 
-
-
 	for r in range(0, grid.shape[0]) :
 		for c in range(0, grid.shape[1]) :
 			chanNum = int(grid[r,c])
@@ -43,28 +42,29 @@ def map_event_data_to_grid() :
 				if eventIndicesForChan.shape[0] > 0 :
 
 					eventGrid[eventIndicesForChan,r,c] = 1		
-	
+
 	return eventGrid
 
 
 
-def convert_event_data_to_grid() :
+def map_event_data_to_grid_with_trailing_edge() :
 
 	eventDataInGrid = map_event_data_to_grid()
-	eventDataWithTrailingEdge = make_gaussian_3d(eventDataInGrid)
+	eventDataWithTrailingEdge = np.round(make_gaussian_3d(eventDataInGrid),3)
 
-	return eventDataWithTrailingEdge
+	normaliseData = np.apply_along_axis(preprocessing.MinMaxScaler().fit_transform, 0, eventDataWithTrailingEdge)
 
+	# print("Normalise 2 Data: ", normaliseData[1:50,:,:])
 
+	# normaliseData = np.linalg.norm(eventDataWithTrailingEdge)
+	# print("Normalise 2 Data: ", normaliseData[1:50,:,:])	
 
-## =========================
-## calc time range of array
-## =========================
+	return normaliseData
+
 
 ## Put a nice gaussian filter around the 1
 def make_gaussian_3d(timeXYarr):
-	sigmaVal = 12 #trailing edge length    
+	sigmaVal = 8 #trailing edge length    
 	gaussian3d = np.apply_along_axis(gaussian_filter, 0, timeXYarr, sigmaVal)
 	# VY = gaussian_filter(VY, sigma=1)
-	gaussian3d = np.multiply(gaussian3d, timeXYarr)
 	return gaussian3d

signal_processing/preprocessing.py
@@ -8,7 +8,7 @@ import numpy as np
 import scipy.io as sio
 
 import config_global as cg
-from sklearn.preprocessing import scale # for normalisation between 0 and 1
+from sklearn import preprocessing # for normalisation between 0 and 1
 
 
 
@@ -35,10 +35,11 @@ def normalise_chan_data(chanData) :
 	clippedData = norm_second_clip(norm_first_clip(chanData,-2000, 2000))
 
 	# Classic normalisation [0,1]
-	normedData = (clippedData - np.max(clippedData))/-np.ptp(clippedData)
-	print("normedData.shape: ", normedData.shape)
 
-	return normedData
+	normaliseData = np.apply_along_axis(preprocessing.MinMaxScaler().fit_transform, 1, clippedData)
+
+
+	return normaliseData
 
 
 